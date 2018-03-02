5602bae027dfe3b8240516cf3f83a390c82442d1
yiyi098 <yiyi098@gmail.com>
2018-03-02 09:25:11 -0800

    test

--- db/schema.sql
+++ db/schema.sql
@@ -10,6 +10,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP,
+	date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
