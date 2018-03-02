8882cd2987765401d3eff0dc377883e7571d1127
yiyi098 <yiyi098@gmail.com>
2018-03-01 22:07:37 -0800

    added heroku JAWSDB connection and fixed readme

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+  // Make connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+  // Export connection for our ORM to use.
+  module.exports = connection;
