21b1b37afe1f288f8241335f28ccb6d8f5bf1e5d
yiyi098 <yiyi098@gmail.com>
2018-03-01 22:36:41 -0800

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -15,15 +15,16 @@ if (process.env.JAWSDB_URL) {
     password: "",
     database: "burgers_db"
   });
+}
 
-  // Make connection.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-  // Export connection for our ORM to use.
-  module.exports = connection;
+// Export connection for our ORM to use.
+module.exports = connection;
